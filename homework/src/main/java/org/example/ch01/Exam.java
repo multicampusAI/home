fd745939533c97154d094bf125a3d2049da102bb
carami <carami@nate.com>
2024-03-18 10:44:04 +0900

    과제완성

---
+++ homework/src/main/java/org/example/ch01/Exam.java
@@ -0,0 +1,8 @@
+package org.example.ch01;
+
+public class Exam {
+    public static void main(String[] args) {
+        System.out.println("과제 코드 작성");
+        System.out.println("과제완료!!");
+    }
+}
